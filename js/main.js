9c31e40fad90ff96a74cd2671410f904a8d599c8
beingkmr <165479365+beingkmr@users.noreply.github.com>
2026-05-28 11:08:06 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function () {
+  var toggle = document.querySelector('.nav-toggle');
+  var links = document.querySelector('.nav-links');
+  if (toggle && links) {
+    toggle.addEventListener('click', function () {
+      links.classList.toggle('open');
+    });
+  }
+  var year = document.getElementById('currentYear');
+  if (year) year.textContent = new Date().getFullYear();
+})();
